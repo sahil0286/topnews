7380d3331b76658bb92d1779150eb63e9592fd02
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-07 00:42:09 +0530

    Update Nwesitem.js

--- src/components/Nwesitem.js
+++ src/components/Nwesitem.js
@@ -4,10 +4,10 @@ export default function Nwesitem(props) {
   return (
     <>
       <div className="card my-3" style={{ width: "20rem" }}>
-        <img src={props.imgurl} className="card-img-top" alt={props.title} />
+        <img src={props.imgurl} className="card-img-top" style={{ height: "200px" , objectFit: "cover"}} alt={props.title} />
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
-          <p className="card-text">{props.desc}</p>
+          <p className="card-text" style={{ maxWidth: "200px"}}>{props.desc}</p>
           <a href={props.url} className="btn btn-primary">
             Read More
           </a>
