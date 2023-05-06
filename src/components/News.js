f38979ad21bd9866204376299b9df95085740b5d
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-06 23:50:31 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Nwesitem from './Nwesitem';
 
 export default function News() {
+    const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=2c879e2305cb4e4db847d40633b2a3c4')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <>
-    
+        <ul>
+      {data.map((item) => {
+        <Nwesitem title={item.title} desc={item.description}/>
+      })}
+    </ul>
     </>
   )
 }
