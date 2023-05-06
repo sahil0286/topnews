f64941e0cc4cd0627c461bb1cb320a7693ab5abd
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-07 00:42:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
     <div className='container my-5'>
       <h2>Top News</h2>
       <div className='row my-3'>
-        <div className='col-md-4'>
           <News/>
-        </div>
         </div>
       </div>
     </>
