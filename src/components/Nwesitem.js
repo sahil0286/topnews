635265411363a41c70cbea3e9c2faf47ea7971f8
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-06 23:50:37 +0530

    Update Nwesitem.js

--- src/components/Nwesitem.js
+++ src/components/Nwesitem.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function Nwesitem() {
+export default function Nwesitem(props) {
   return (
     <>
     <div className="card" style={{width: "20rem"}}>
   <img src="..." className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 className="card-title">{props.title}</h5>
+    <p className="card-text">{props.desc}</p>
     <a href="/" className="btn btn-primary">Go somewhere</a>
   </div>
 </div>
