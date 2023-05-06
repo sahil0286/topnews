8dc7dea5e1790e112b8a2ab6700e094dc8e3f5d7
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-06 23:53:38 +0530

    Fix

--- src/components/Nwesitem.js
+++ src/components/Nwesitem.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Nwesitem(props) {
   return (
     <>
-    <div className="card" style={{width: "20rem"}}>
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{props.title}</h5>
-    <p className="card-text">{props.desc}</p>
-    <a href="/" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+      <div className="card" style={{ width: "20rem" }}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.desc}</p>
+          <a href="/" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
     </>
-  )
+  );
 }
