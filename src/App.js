61653896a12070ebe68165c7561845002dbd100a
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-06 23:14:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Nwesitem from './components/Nwesitem';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Nwesitem/>
     </>
   );
 }
