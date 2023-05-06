b0b591c01efaee0c0c2255bb09bf8ec3992ac105
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-06 23:15:30 +0530

    Update Nwesitem.js

--- src/components/Nwesitem.js
+++ src/components/Nwesitem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Nwesitem() {
   return (
     <>
-    <div className="card" style="width: 18rem;">
+    <div className="card" style={{width: "18rem"}}>
   <img src="..." className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
