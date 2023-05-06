652a36cda247d14c624f97c6a60f5e5ccdc06763
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-06 23:54:52 +0530

    Update Nwesitem.js

--- src/components/Nwesitem.js
+++ src/components/Nwesitem.js
@@ -9,7 +9,7 @@ export default function Nwesitem(props) {
           <h5 className="card-title">{props.title}</h5>
           <p className="card-text">{props.desc}</p>
           <a href="/" className="btn btn-primary">
-            Go somewhere
+            Go somewher
           </a>
         </div>
       </div>
