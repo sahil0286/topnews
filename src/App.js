a6cd12fc8d9f3b4f700cf11e67f14983c46e4169
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-06 23:32:45 +0530

    App fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ function App() {
   return (
     <>
     <Navbar/>
-    <Nwesitem/>
+    <div className='container my-5'>
+      <h2>Top News</h2>
+      <div className='row my-3'>
+        <div className='col-md-4'>
+          <Nwesitem/>
+        </div>
+        <div className='col-md-4'>
+          <Nwesitem/>
+        </div>
+        <div className='col-md-4'>
+          <Nwesitem/>
+        </div>
+      </div>
+    </div>
     </>
   );
 }
