8d2b84d4c797ca9bc3eb5045fbda95de6d5dc85c
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-07 11:04:21 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import Nwesitem from './Nwesitem';
+import axios from 'axios';
 
 export default function News() {
     const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=2c879e2305cb4e4db847d40633b2a3c4')
-      .then(response => response.json())
-      .then(data => setData(data.articles))
-      .catch(error => console.error(error));
-  }, []);
+    useEffect(() => {
+      axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=2c879e2305cb4e4db847d40633b2a3c4')
+  .then(function (response) {
+    // handle success
+    setData(response.data.articles)
+    console.log(response.data.articles);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+    }, [])
+    
+
+  // useEffect(() => {
+  //   fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=2c879e2305cb4e4db847d40633b2a3c4')
+  //     .then(response => response.json())
+  //     .then(data => setData(data.articles))
+  //     .catch(error => console.error(error));
+  // }, []);
 
   return (
     <>
@@ -18,8 +33,7 @@ export default function News() {
         {data.map(item => (
             // <li key={item.id}>{item.name}</li>
             <div className='col-md-4'>
-
-                <Nwesitem title={item.title} desc={item.description} url={item.url} imgurl={item.urlToImage}/>
+                <Nwesitem key={item.key} title={item.title.slice(0, 80)+" ..."} desc={item.description} url={item.url} imgurl={item.urlToImage}/>
             </div>
             ))}
     </div>
