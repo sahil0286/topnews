0e832c7f1c58fec240725908c2199e49438f9376
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-08 14:25:31 +0530

    Create Spinner.js

---
+++ src/components/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Spinner from "./media/Spinner.gif"
+
+export default function Spinner() {
+  return (
+    <div>
+        <img src={Spinner} alt="Spinner" />
+    </div>
+  )
+}
