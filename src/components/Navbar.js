6cb884a9ea837e9a84a7e65ab845504b44cee690
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-08 16:03:19 +0530

    Fix navnar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import iconavbar from "./media/iconavbar.png"
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
@@ -7,17 +8,17 @@ export default function Navbar() {
       <header className="p-3 text-bg-dark">
     <div className="container">
       <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/general" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <img className="bi me-2" height="45" role="img" aria-label="Bootstrap" src={iconavbar} alt="" />
-        </a>
+        <Link to="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <img className="bi me-2" height="45" role="img" aria-label="topnews" src={iconavbar} alt="" />
+        </Link>
 
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="/technology" className="nav-link px-2 text-white">Technology</a></li>
-          <li><a href="/sports" className="nav-link px-2 text-white">Sports</a></li>
-          <li><a href="/science" className="nav-link px-2 text-white">Science</a></li>
-          <li><a href="/health" className="nav-link px-2 text-white">Health</a></li>
-          <li><a href="/business" className="nav-link px-2 text-white">Business</a></li>
-          <li><a href="/entertainment" className="nav-link px-2 text-white">Entertainment</a></li>
+          <li><Link to="/technology" className="nav-link px-2 text-white">Technology</Link></li>
+          <li><Link to="/sports" className="nav-link px-2 text-white">Sports</Link></li>
+          <li><Link to="/science" className="nav-link px-2 text-white">Science</Link></li>
+          <li><Link to="/health" className="nav-link px-2 text-white">Health</Link></li>
+          <li><Link to="/business" className="nav-link px-2 text-white">Business</Link></li>
+          <li><Link to="/entertainment" className="nav-link px-2 text-white">Entertainment</Link></li>
         </ul>
 
         <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
