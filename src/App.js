cf2487ac4895466b52e9b827542287aa5fa89f57
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-08 21:43:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,20 +13,38 @@ function App() {
   document.body.style.color = "black";
   return (
     <>
-    <Router>
-      <Navbar/>
-      <Switch>
-          <Route exact path="/" key="general" element={<News pageSize="9" category="general" />}></Route>
-          <Route exact path="/technology" key="technology" element={<News pageSize="9" category="technology" />}></Route>
-          <Route exact path="/sports" key="sports" element={<News pageSize="9" category="sports" />}></Route>
-          <Route exact path="/science" key="science" element={<News pageSize="9" category="science" />}></Route>
-          <Route exact path="/health" key="health" element={<News pageSize="9" category="health" />}></Route>
-          <Route exact path="/business" key="business" element={<News pageSize="9" category="business" />}></Route>
-          <Route exact path="/entertainment" key="entertainment" element={<News pageSize="9" category="entertainment" />}></Route>
-      </Switch>
-      <Footer/>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <News pageSize="9" category="general" />
+          </Route>
+          <Route exact path="/technology">
+            <News pageSize="9" category="technology" />
+          </Route>
+          <Route exact path="/sports">
+            <News pageSize="9" category="sports" />
+          </Route>
+          <Route exact path="/science">
+            <News pageSize="9" category="science" />
+          </Route>
+          <Route exact path="/health">
+            <News pageSize="9" category="health" />
+          </Route>
+          <Route exact path="/business">
+            <News pageSize="9" category="business" />
+          </Route>
+          <Route exact path="/entertainment">
+            <News pageSize="9" category="entertainment" />
+          </Route>
+          <Route path="*">
+            <h1 className='text-center my-5'>404 Not Found</h1>
+          </Route>
+        </Switch>
+        <Footer />
       </Router>
     </>
   );
 }
+
 export default App;
