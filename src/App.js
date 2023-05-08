b7e43bb9d32df1cfd118957f4c922dd86bc74b8c
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-08 16:02:44 +0530

    Fix app

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   document.body.style.backgroundColor = "#7A8289";
   document.body.style.color = "black";
   return (
     <>
-    <Navbar/>
-      <div className='container my-5'>
-        <div className='row my-3'>
-          <News pageSize="3"/>
-        </div>
-      </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+          <Route path="/technology" element={<News pageSize="3"/>}>
+          </Route>
+      </Routes>
       <Footer/>
+      </Router>
     </>
   );
 }
