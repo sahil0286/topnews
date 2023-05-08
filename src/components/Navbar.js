1183bd26d8304760fc959b6d9e6f9d8504cb16d0
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-08 15:41:26 +0530

    fix navebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,16 +7,17 @@ export default function Navbar() {
       <header className="p-3 text-bg-dark">
     <div className="container">
       <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+        <a href="/general" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
           <img className="bi me-2" height="45" role="img" aria-label="Bootstrap" src={iconavbar} alt="" />
         </a>
 
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="/" className="nav-link px-2 text-white">Home</a></li>
-          <li><a href="/" className="nav-link px-2 text-white">Features</a></li>
-          <li><a href="/" className="nav-link px-2 text-white">Pricing</a></li>
-          <li><a href="/" className="nav-link px-2 text-white">FAQs</a></li>
-          <li><a href="/" className="nav-link px-2 text-white">About</a></li>
+          <li><a href="/technology" className="nav-link px-2 text-white">Technology</a></li>
+          <li><a href="/sports" className="nav-link px-2 text-white">Sports</a></li>
+          <li><a href="/science" className="nav-link px-2 text-white">Science</a></li>
+          <li><a href="/health" className="nav-link px-2 text-white">Health</a></li>
+          <li><a href="/business" className="nav-link px-2 text-white">Business</a></li>
+          <li><a href="/entertainment" className="nav-link px-2 text-white">Entertainment</a></li>
         </ul>
 
         <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
