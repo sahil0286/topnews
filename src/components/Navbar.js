94168558d17791506dcb772a267d6decfde83ab2
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-10 23:10:57 +0530

    Top Sticky

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <>
-      <header className="p-3 text-bg-dark">
+      <header className="p-3 text-bg-dark sticky-top">
     <div className="container">
       <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <Link to="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
