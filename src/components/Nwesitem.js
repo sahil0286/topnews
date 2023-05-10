035186ce60f22862c34e34b09e833dac0f2fa0f3
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-10 23:07:51 +0530

    Update Nwesitem.js

--- src/components/Nwesitem.js
+++ src/components/Nwesitem.js
@@ -8,10 +8,12 @@ export default function Nwesitem(props) {
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
           <p className="card-text" >{props.desc}</p>
-          <h6>Author : {props.author}</h6>
           <a href={props.url} className="btn btn-primary">
             Read More
           </a>
+          <div class=" mx-2">
+      <small class="text-body-secondary">Author : {props.author}</small>
+    </div>
         </div>
       </div>
     </>
